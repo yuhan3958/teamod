279b57be6878d5e4fc26b60475f65d4d46480012
yuhan8954 <89955275+yuhan3958@users.noreply.github.com>
2025-11-21 00:11:07 +0900

    TeamodClient.java 업데이트

--- src/client/java/me/yuhan8954/TeamodClient.java
+++ src/client/java/me/yuhan8954/TeamodClient.java
@@ -6,5 +6,6 @@ public class TeamodClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+          BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.TEA_PLANT, RenderType.cutout());
 	}
 }
