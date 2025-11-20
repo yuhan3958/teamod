8880af8ac435829a2b26702a4a980a92f617f9f3
yuhan8954 <89955275+yuhan3958@users.noreply.github.com>
2025-11-21 08:41:03 +0900

    Add block rendering imports to TeamodClient

--- src/client/java/me/yuhan8954/TeamodClient.java
+++ src/client/java/me/yuhan8954/TeamodClient.java
@@ -1,6 +1,9 @@
 package me.yuhan8954;
 
 import net.fabricmc.api.ClientModInitializer;
+import me.yuhan8954.block.ModBlocks;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
 
 public class TeamodClient implements ClientModInitializer {
 	@Override
